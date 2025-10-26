2548fab1819a5da04f08edfeeccd22e108cf55e3
Yazkan1 <lenngrowth2@gmail.com>
2025-10-26 23:59:16 +0200

    Update PoweredBy.vue

--- packages/frontend/@n8n/chat/src/components/PoweredBy.vue
+++ packages/frontend/@n8n/chat/src/components/PoweredBy.vue
@@ -1,7 +1,7 @@
 <template>
-	<div class="chat-powered-by">
+	<div class="">
 		Powered by
-		<a href="https://n8n.io?utm_source=n8n-external&utm_medium=widget-powered-by">n8n</a>
+		<a href="https://n8n.io?utm_source=n8n-external&utm_medium=widget-powered-by"></a>
 	</div>
 </template>
 
